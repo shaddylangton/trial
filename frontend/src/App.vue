4e9a3435fde8f9a123905e9bead3a2df8b819047
shaddylangton <39008572+shaddylangton@users.noreply.github.com>
2022-12-02 16:20:59 +0300

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <MetaMaskLogin />
+</template>
+
+<script setup>
+  import MetaMaskLogin from "@/components/MetaMaskLogin.vue";
+
+ 
+</script>
+
+
+<style>
+@import 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@500;700&display=swa';
+@import 'https://fonts.gstatic.com';
+@import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css';
+@import 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css';
+@import './assets/lib/owlcarousel/assets/owl.carousel.min.css';
+@import './assets/css/bootstrap.min.css';
+@import './assets/css/style.css';
+@import 'https://code.jquery.com/jquery-3.4.1.min.js';
+@import 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js';
+
+</style>
